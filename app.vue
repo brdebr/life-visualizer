e032e67be294c6aeb488ad79815424e1ad1da446
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-23 23:11:01 +0100

    Install Tailwind Nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container mx-auto min-h-[100dvh]">
+    <h1 class="text-slate-600">Hello world</h1>
+    <p>From my Nuxt App</p>
   </div>
 </template>
