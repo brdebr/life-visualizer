bc70df91daafae5aaa1c61eb44f8230e7948f1d7
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-26 12:01:14 +0100

    Improve EventTolltip

--- components/EventTooltip.vue
+++ components/EventTooltip.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class='border border-blue-900 rounded-md max-w-[420px] mx-auto my-3 px-3 pt-1 pb-2'>
-    <h3 class="px-2 pt-1 mb-2 border-b text-sm">{{selectedEvent?.eventDate || ''}}</h3>
+  <div class='border border-blue-900 rounded-md max-w-[420px] min-w-[300px] mx-auto my-3 px-3 pt-1 pb-2'>
+    <h3 class="px-2 pt-1 mb-2 border-b text-sm flex">
+      <span>
+        {{selectedEvent?.eventDate || ''}}
+      </span>
+      <span class="ml-auto pl-1">
+        {{ fromNow }}
+      </span>
+    </h3>
     <h4 class="font-semibold text-sm">· {{selectedEvent?.title || 'No events this day'}}</h4>
     <p class="p-2 text-sm">{{selectedEvent?.description || ''}}</p>
   </div>
@@ -8,4 +15,5 @@
 <script setup lang="ts">
 const appStore = useAppStore();
 const { selectedEvent } = storeToRefs(appStore);
+const fromNow = computed(() => selectedEvent.value?.eventDate ? appStore.dayjs(selectedEvent.value.eventDate).fromNow() : '');
 </script>
