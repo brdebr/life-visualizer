bb5059efc8fef24f2312f50c0d9318e226a03926
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-27 12:39:01 +0100

    Use logo as icon and minor fixes

--- components/Heading.vue
+++ components/Heading.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1 class="text-slate-800 text-xl font-semibold tracking-wide mb-2 text-center pl-5">Life Visualizer ⌛</h1>
+  <h1 class="text-slate-800 text-4xl font-semibold tracking-wide mb-6 text-center pl-5 flex items-center justify-center gap-2">
+    <span>
+      Life Visualizer
+    </span>
+    <img class="inline-block mt-1 ml-3" src="/logo-icon-bg.png" alt="logo" width="18" height="18" />
+  </h1>
 </template>
