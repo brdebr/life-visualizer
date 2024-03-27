00fedfd9f8aef55f42ff6dc62e6b5e9894597c75
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2024-03-27 09:43:52 +0100

    Add thumbnail

--- app.vue
+++ app.vue
@@ -13,7 +13,12 @@ useHead({
   ],
   link: [
     { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2296%22>'+faviconEmoji+'</text></svg>' }
-  ]
+  ],
+})
+useSeoMeta({
+  ogTitle: 'Life Visualizer',
+  ogDescription: 'Little project to visualize your whole life like a Github contributions heatmap',
+  ogImage: '/thumbnail.jpg',
 })
 </script>
 <style lang="scss">
