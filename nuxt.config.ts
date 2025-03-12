c2ec807cf4f9665fd908b9758aef145955d93a24
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2025-03-12 08:07:10 +0100

    Add umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,13 @@ export default defineNuxtConfig({
     checker: true,
   },
   ssr: false,
+  app: {
+    head: {
+      script: [
+        { 'src': 'https://analytics.io.bryan-web.dev/script.js', 'defer': true, 'data-website-id': '9ed946b1-9576-4798-890d-ce6e6b30f2ab' },
+      ],
+    },
+  },
   colorMode: {
     preference: 'light',
     fallback: 'light',
