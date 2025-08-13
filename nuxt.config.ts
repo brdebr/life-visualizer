b03531108561e51f1f2ad3bf439d0610f1b22b0f
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2025-08-13 22:53:27 +0200

    Fix tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui'],
-  css: ['~/assets/styles/global.scss'],
+  css: ['~/assets/styles/global.css'],
   eslint: {
     config: {
       stylistic: true
